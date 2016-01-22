b4bb2b61c45bf147e6bb8258868651b4e7dd9008
Robert Handy <robby.handy@gmail.com>
2016-01-22 14:06:48 -0800

    lots of stuff and added Alamofire

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,41 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Robert on 1/21/16.
+//  Copyright © 2016 Robert Handy. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenceLbl: UILabel!
+    @IBOutlet weak var hightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evLbl: UILabel!
+    
+    
+    
+    
+    var pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLbl.text = pokemon.name
+    }
+
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
